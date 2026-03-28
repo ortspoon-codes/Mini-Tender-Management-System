0509c29a7aa28010a5043109e3b153d63dd50418
ortspoon-codes <vedantkothari25@gmail.com>
2026-03-28 15:10:40 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mini Tender Management System");
+
+        Scanner sc = new Scanner(System.in);
+        TenderService ts = new TenderService();
+
+        while (true) {
+            System.out.println("\n1. Add Tender\n2. View Tenders\n3. Exit");
+            int choice = sc.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Enter ID: ");
+                int id = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Title: ");
+                String title = sc.nextLine();
+
+                System.out.print("Description: ");
+                String desc = sc.nextLine();
+
+                System.out.print("Deadline: ");
+                String dl = sc.nextLine();
+
+                ts.addTender(new Tender(id, title, desc, dl));
+            } else if (choice == 2) {
+                ts.viewTenders();
+            } else {
+                break;
+            }
+        }
     }
 }
