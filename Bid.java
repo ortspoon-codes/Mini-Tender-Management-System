834fbd8a4c92719b76c4deb00b360a84ce2b34a3
ortspoon-codes <vedantkothari25@gmail.com>
2026-03-28 15:13:44 +0530

    Added bidding system for vendors

--- Bid.java
+++ Bid.java
@@ -2,4 +2,10 @@ class Bid {
     int tenderId;
     String vendorName;
     double amount;
+
+    public Bid(int tenderId, String vendorName, double amount) {
+        this.tenderId = tenderId;
+        this.vendorName = vendorName;
+        this.amount = amount;
+    }
 }
